c9f9e0855114c78940730ce7dbd21cce27335347
Miles Hill <ec2-user@ip-172-31-52-216.ec2.internal>
2016-09-29 20:45:36 +0000

    New ppl run

--- pjm/peco/run_calcs.sh
+++ pjm/peco/run_calcs.sh
@@ -37,66 +37,116 @@ if [ -z "${UTILITY}" ]; then
     UTILITY=$(basename $(pwd))
 fi
 END
+#################### Var Declaration #################### 
+# Utility Level Vars
 UTILITY=$(basename $(pwd))
-# Variable declarition
-RECIPE="${UTILITY}_rec.m"
-RECIPE_RESULT="${UTILITY}_rec.csv"
-UNIQ_PREM="${UTILITY}_premises.txt"
-PREDICTION="${UTILITY}_pred_most.m"
-PREDICTION_RESULT="${UTILITY}_pred_most.csv"
 LOGFILE="./log/${UTILITY}.log"
 OVERWRITE=0;
-
 RESULT_DIR=./results/
+
+# Interval
+INT_REC="${UTILITY}_rec.m"
+INT_REC_RES="${UTILITY}_rec.csv"
+INT_PRED="${UTILITY}_pred_most.m"
+INT_PRED_RES="${UTILITY}_pred_most.csv"
+INT_UNIQ="${UTILITY}_premises.txt"
+
+# Monthly demand
+DMD_REC="${UTILITY}_rec_dmd.m"
+DMD_REC_RES="${UTILITY}_rec_dmd.csv"
+DMD_PRED="${UTILITY}_pred_dmd.m"
+DMD_PRED_RES="${UTILITY}_pred_dmd.csv"
+DMD_UNIQ="${UTILITY}_premises_dmd.txt"
+
+:<<'END'
+# Monthly consumption
+CON_REC="${UTILITY}_rec_con.m"
+CON_REC_RES="${UTILITY}_rec_con.csv"
+CON_PRED="${UTILITY}_pred_con.m"
+CON_PRED_RES="${UTILITY}_pred_con.csv"
+CON_UNIQ="${UTILITY}_premises_con.txt"
+END
+
+# Could this be junked?
 COMPARE="pred_compare.m"
 COMPARE_RESULT="${UTILITY}_pred_compare.csv"
 
+#################### Auxiliary Functions ####################  
 # Auxiliary functions
 function record {
     echo "CST:$(date '+%Y_%m_%d_%H_%M_%S') UTILITY=${UTILITY} DESC=$1" >> ${LOGFILE}
 }
 
-:<<'END'
-# If clean directory
-if [ "${OVERWRITE}" ]; then
-    find . -type f -not \( -name "*.m" -o -name "*.sh" \) -print0 | xargs -0 rm -f 
-fi
-END
+# recipe_calc
+# Computes RECIPE, stores in RESULT, and determines UNIQue premies; UNQ passed to predict  
+function recipe_calc {
+    RECIPE="$1"
+    RESULT="$2"
+    UNQ="$3"
+    if [ ! -e "${RESULT}" ]; then 
+        # run recipe and store result
+        $(record "Recipe start")
+        MathKernel -script ${RECIPE} > ${RESULT}
+        # store unique premises
+        $(record "Recipe end")
+        # print premise id and store uniques
+        cat ${RESULT} | awk -F ',' '{print $3}' | sort -n | uniq  > ${UNQ}
+        $(record "Unique premises: $(wc -l < ${UNQ})")
+    fi
+}
 
+# predict:
+# PREDicts  icap UNIQue for premises and pipes into RESULT
+function predict {
+    PRED="$1"
+    RESULT="$2"
+    UNQ="$3"
+    if [ -e "${UNQ}" ] ; then
+        $(record "Prediction start")
+        $(record "Splitting premises")
+        sh split_premises.sh ${UNQ}
+        touch ${RESULT}
+        find . -maxdepth 1 -name "prem_*" -print | xargs -n 1 -P 2 -I {} sh -c "MathKernel -script ${PRED} {} >> ${RESULT}" 
+        # check to ensure WolframKernels have terminated; remove split premise files; prem_*
+        if [ ! "$(pgrep Wolfram)" ]; then
+            rm prem_*
+        fi
+        $(record "Prediction end")
+    fi
+}
+
+:<<'END'
+#################### Initialization Logic #################### 
 # Does the recipe and prediction scripts exist?
 if [ ! -e "${RECIPE}" ] && [ ! -e "${PREDICTION}" ] ; then
     $(record "No scripts found")
     exit
 fi
+END
 
+#################### Recipe #################### 
+# Interval recipe and uniq premises
+$(recipe_calc ${INT_REC} ${INT_REC_RES} ${INT_UNIQ})
 
-# Has the recipe been run?
-if [ ! -e "${RECIPE_RESULT}" ]; then 
-    # run recipe and store result
-    $(record "Recipe start")
-    MathKernel -script ${RECIPE} > ${RECIPE_RESULT}
-    # store unique premises
-    $(record "Recipe end")
-    cat ${RECIPE_RESULT} | awk -F ',' '{print $3}' | sort -n | uniq  > ${UNIQ_PREM}
-    $(record "Unique premises: $(wc -l < ${UNIQ_PREM})")
-fi
+# Monthly with demand and uniq premises
+$(recipe_calc ${DMD_REC} ${DMD_REC_RES} ${DMD_UNIQ})
 
-# Split the premise data and run predictions 
-if [ -e "${UNIQ_PREM}" ] ; then
-    $(record "Prediction start")
-    $(record "Splitting premises")
-    sh split_premises.sh ${UNIQ_PREM}
-    touch ${PREDICTION_RESULT}
-    find . -maxdepth 1 -name "prem_*" -print | xargs -n 1 -P 2 -I {} sh -c "MathKernel -script ${PREDICTION} {} >> ${PREDICTION_RESULT}" 
-    # check to ensure WolframKernels have terminated; remove split premise files; prem_*
-    if [ ! "$(pgrep Wolfram)" ]; then
-        rm prem_*
-    fi
-    echo "pred end"
-    $(record "Prediction end")
-fi
+# Monthly consumption and uniq premises
+#$(recipe_calc ${CON_REC} ${CON_REC_RES} ${CON_UNIQ})
 
-# Move results to ./Results
+#################### Predictions #################### 
+# Interval 
+$(predict ${INT_PRED} ${INT_PRED_RES} ${INT_UNIQ})
+
+# Monthly with demand
+$(predict ${DMD_PRED} ${DMD_PRED_RES} ${DMD_UNIQ})
+
+# Monthly consumption
+#$(predict ${CON_PRED} ${CON_PRED_RES} ${CON_UNIQ})
+
+#################### Cleanup and S3 Export #################### 
+# cat recipe results; cat prediction results; move to ./results; send to S3
+
 if [ ! -d "${RESULT_DIR}" ]; then
    $(record "Create ${RESULT_DIR}; move *.csv *.txt")
    mkdir ${RESULT_DIR} && mv -u *.csv *.txt ${RESULT_DIR}
--- pjm/peco/split_premises.sh
+++ pjm/peco/split_premises.sh
@@ -14,11 +14,7 @@ function add_date(){
 
 
 if [ -f "$1" ]; then
-    echo "$(add_date) Splitting file: ${1}"
     LINE_COUNT="$(wc -l < ${1})"
-    echo "$(add_date) The line count is: ${LINE_COUNT}"
     SPLIT_SIZE="$(( ${LINE_COUNT} / 2 ))"
-    echo "$(add_date) The split size is: ${SPLIT_SIZE}"
     split -a 1 -l ${SPLIT_SIZE} $1 prem_
-    tree
 fi
