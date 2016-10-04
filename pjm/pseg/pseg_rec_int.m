8841dcb344e0ef00e67a62c9954c48255025a3c7
Miles Hill <ec2-user@ip-172-31-52-216.ec2.internal>
2016-10-04 12:23:16 +0000

    PSEG Interval Recipe

---
+++ pjm/pseg/pseg_rec_int.m
@@ -0,0 +1,99 @@
+#!/usr/bin/local/WolframScript -script
+
+BeginPackage["PSEG`", {"DatabaseLink`", "DBConnect`"}];
+
+
+(* #################### Queries #################### *)
+recordQuery = "select h.PremiseId, 
+		Cast(Year(h.UsageDate) as varchar) as Year, 
+		RTrim(p.RateClass), RTrim(p.Strata),
+		h.Usage
+	from HourlyUsage as h
+	inner join CoincidentPeak as c
+		on  c.UtilityId = h.UtilityId 
+		and c.CPDate = h.UsageDate
+		and c.HourEnding = h.HourEnding
+	inner join Premise as p
+		on p.UtilityID = h.UtilityId
+		and p.PremiseId = h.PremiseID
+	where h.UtilityId = 'PSEG'
+	order by h.PremiseId, Year";
+
+
+utilityQuery = "select distinct 
+		CAST((c.CPYearID - 1) as varchar), 
+		RTrim(u.RateClass), RTrim(u.Strata),
+		u.ParameterValue
+	from UtilityParameterValue as u
+	inner join CoincidentPeak as c
+		on c.CPID = u.CPID
+	where u.UtilityId = 'PSEG'
+		and u.RateClass like '%-INT'";
+
+systemQuery = "select Cast(CPYearId as varchar), ParameterValue
+	from SystemLoad
+	where UtilityId = 'PSEG'
+		and ParameterId = 'CapObligScale'";
+
+(* #################### Execute Queries #################### *)
+conn = JEConnection[];
+If[Not @ MatchQ[conn, _SQLConnection],
+    Throw[$Failed]; Return[1],
+    Nothing
+];
+
+records = SQLExecute[conn, recordQuery]//
+	<|"Premise" -> #, "Year" -> #2, "RateClass" -> #3, "Strata" -> #4, "Usage" -> #5|>& @@@ #& //
+	GroupBy[#, {#Premise, #Year, #RateClass, #Strata}&]& //
+	Map[Merge[Identity]] //
+	Map[#Usage&]//
+	Normal //
+	# /. Rule[key_, usage_] :> Flatten[{key, usage}]& //
+	Select[#, Length @ # == 9&]&;
+
+
+(* {year, rateclass, strata} -> paramvalue *)
+util = SQLExecute[conn, utilityQuery]//
+	MapAt[First @ StringSplit[#,"-"]&, #, {All, 2}]& //
+	<|"Year" -> #1, "RateClass" -> #2, "Strata" -> #3, "PV" -> ToExpression @ #4|>& @@@ #& //
+	GroupBy[#, {#Year, #RateClass}&]& //
+	Map[Merge[Identity]]//
+	Map[(Times @@ #PV)&];
+
+(* {year} -> paramvalue *)
+sys = SQLExecute[conn, systemQuery]//
+	Rule[#, #2]& @@@ #&//
+	Association;
+
+missingUtil = <| "2014" -> (1.0913 * 1.02800111), "2015"-> (1.0952 * 1.06246338)|>;
+
+(* #################### Compute ICap #################### *)
+stdout = Streams[][[1]];
+writeFunc = Write[stdout, StringRiffle[#, ","]]&;
+
+labels = {"PremiseId", "Year", "RateClass", "Strata", "ICap"};
+writeFunc @ labels;
+
+Do[
+
+    {premId, year, rc, st, usage} = {#, #2, StringSplit[#3,"-"][[1]], #4, {##5}}& @@ record; 
+	
+	utilFactor = Lookup[util, {{year, rc}}, 0.] // If[MatchQ[#, _List], First @ #]&;
+	sysFactor = Lookup[sys, year, 0.];
+	missingFactor = Lookup[missingUtil, year, 0.];
+	
+	scalar = Times @@ {utilFactor, sysFactor, missingFactor};
+	
+	icap = Mean[usage * scalar];
+	results = {premId, year, rc, st, icap};
+	
+	writeFunc @ results;
+
+    ,{record, records}];
+
+
+
+   
+
+EndPackage[];
+
---
+++ pjm/pseg/run_calcs.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+# Use -gt 1 to consume two arguments per pass in the loop (e.g. each
+# argument has a corresponding value to got with it.)
+
+# Logging and overwrite vars
+
+:<<'END'
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -o |--overwrite)
+            OVERWRITE=1
+            shift
+            ;;
+        -p|--predict)
+            ONLY_PREDICT=1
+            shift
+            ;;
+        -r|--recipe)
+            ONLY_RECIPE=1
+            shift
+            ;;
+        -u |--utility)
+            UTILITY="$2"
+            shift
+            ;;
+        *)
+            ;;
+    esac
+    shift
+done
+
+# If no utility given
+
+if [ -z "${UTILITY}" ]; then
+    UTILITY=$(basename $(pwd))
+fi
+END
+#################### Var Declaration #################### 
+# Utility Level Vars
+UTILITY=$(basename $(pwd))
+LOGFILE="./log/${UTILITY}.log"
+OVERWRITE=0;
+RESULT_DIR=./results/
+
+RECIPE_RESULTS="${UTILITY}_rec.csv"
+
+# Interval
+INT_REC="${UTILITY}_rec_int.m"
+INT_REC_RES="${UTILITY}_rec_int.csv"
+INT_PRED="${UTILITY}_pred_most.m"
+INT_PRED_RES="${UTILITY}_pred_most.csv"
+INT_UNIQ="${UTILITY}_premises_int.txt"
+INT_ALIAS="INT"
+
+# Monthly demand
+DMD_REC="${UTILITY}_rec_dmd.m"
+DMD_REC_RES="${UTILITY}_rec_dmd.csv"
+DMD_PRED="${UTILITY}_pred_dmd.m"
+DMD_PRED_RES="${UTILITY}_pred_dmd.csv"
+DMD_UNIQ="${UTILITY}_premises_dmd.txt"
+DMD_ALIAS="DMD"
+
+# Monthly consumption
+CON_REC="${UTILITY}_rec_con.m"
+CON_REC_RES="${UTILITY}_rec_con.csv"
+CON_PRED="${UTILITY}_pred_con.m"
+CON_PRED_RES="${UTILITY}_pred_con.csv"
+CON_UNIQ="${UTILITY}_premises_con.txt"
+CON_ALIAS="CON"
+
+# Could this be junked?
+COMPARE="pred_compare.m"
+COMPARE_RESULT="${UTILITY}_pred_compare.csv"
+
+#################### Auxiliary Functions ####################  
+# Auxiliary functions
+function record {
+    echo "CST:$(date '+%Y_%m_%d_%H_%M_%S') UTILITY=${UTILITY} DESC=$1" >> ${LOGFILE}
+}
+
+# recipe_calc
+# Computes RECIPE, stores in RESULT, and determines UNIQue premies; UNQ passed to predict  
+function recipe_calc {
+    RECIPE="$1"
+    RESULT="$2"
+    UNQ="$3"
+    if [ ! -e "${RESULT}" ]; then 
+        # run recipe and store result
+        $(record "Recipe start")
+        MathKernel -script ${RECIPE} >> ${RESULT}
+        # store unique premises
+        $(record "Recipe end")
+        # print premise id and store uniques
+        cat ${RESULT} | awk -F ',' '{print $3}' | sort -n | uniq  > ${UNQ}
+        $(record "Unique premises: $(wc -l < ${UNQ})")
+        cat ${RESULT} >> ${RECIPE_RESULTS}
+    fi
+}
+
+# predict:
+# PREDicts  icap UNIQue for premises and pipes into RESULT
+function predict {
+    PRED="$1"
+    RESULT="$2"
+    UNQ="$3"
+    ALIAS="$4"
+    if [ -e "${UNQ}" ] ; then
+        $(record "Prediction start")
+        $(record "Splitting premises")
+        sh split_premises.sh ${UNQ} ${ALIAS}
+        touch ${RESULT}
+        find . -maxdepth 1 -name "${ALIAS}*.tmp" -print0 | xargs -0 -n 1 -P 2 -I {} sh -c "MathKernel -script ${PRED} {} >> ${RESULT}" 
+        $(record "Prediction end")
+    fi
+}
+
+#################### Recipe #################### 
+# Interval recipe and uniq premises
+$(recipe_calc ${INT_REC} ${INT_REC_RES} ${INT_UNIQ})
+
+# Monthly with demand and uniq premises
+$(recipe_calc ${DMD_REC} ${DMD_REC_RES} ${DMD_UNIQ})
+
+# Monthly consumption and uniq premises
+$(recipe_calc ${CON_REC} ${CON_REC_RES} ${CON_UNIQ})
+
+#################### Predictions #################### 
+# Interval 
+#$(predict ${INT_PRED} ${INT_PRED_RES} ${INT_UNIQ} ${INT_ALIAS})
+
+# Monthly with demand
+#$(predict ${DMD_PRED} ${DMD_PRED_RES} ${DMD_UNIQ})
+
+# Monthly consumption
+#$(predict ${CON_PRED} ${CON_PRED_RES} ${CON_UNIQ})
+
+#################### Cleanup and S3 Export #################### 
+# cat recipe results; cat prediction results; move to ./results; send to S3
+
+if [ ! -d "${RESULT_DIR}" ]; then
+   $(record "Create ${RESULT_DIR}; move *.csv *.txt")
+   mkdir ${RESULT_DIR} && mv -u *.csv *.txt ${RESULT_DIR}
+else
+    $(record "Move *.csv *.txt to ${RESULT_DIR}")
+    mv -u *.csv *.txt ${RESULT_DIR}
+    find . -maxdepth 1 -type f -name "*.tmp" -print0 | xargs -0 -I {} sh -c "rm {}"
+fi
+
+$(record "Complete")
+echo >> ${LOGFILE}
